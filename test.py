2a4987ed281ef47787dd5eccd2d123cb1c9264b3
Sabin Dcoster <sabin.dcoster@gmail.com>
2021-01-22 04:31:20 +0545

    Testing 2

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("hello world")
+class Testing:
+
+    def setup(params):
+        a = list()
+        for i in range(params):
+            for j in range(params + 1):
+                print(i + j)
+                a.append(i + j)
+     
+    if __name__ == "__main__":
+        setup(100)
