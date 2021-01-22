33a13bdd7f16598b7e066cceb757877345c91811
Sabin Dcoster <sabin.dcoster@gmail.com>
2021-01-22 11:20:18 +0545

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 class Testing:
-
+    
     def setup(params):
-       a = list()
-       for i in range(params):
-       for j in range(params + 1):
-       print(i + j)
-       a.append(i + j)
+        a = list()
+        for i in range(params):
+            for j in range(params + 1):
+                print(i + j)
+                a.append(i + j)
      
     if __name__ == "__main__":
         setup(100)
